bd7f6019833b3aadc2521609646c16e87ba74bc0
Riko <riko.fluchel@gmail.com>
2025-03-28 07:55:37 -0400

    add initial render of 4 sushis and click more button to add more sushis

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,49 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [sushis, setSushis] = useState([])
+  const [displayedSushis, setDisplayedSushis] = useState([])
+  const [displaySetCount, setDisplaySetCount] = useState(1)
+
+  useEffect(() => {
+    fetch(API)
+      .then((r) => r.json())
+      .then((data) => {
+        setSushis(data);
+      })
+  }, [])
+
+  //display initial set of 4 sushis once sushis are loaded in state
+  useEffect(() => {
+    if (sushis.length > 0){
+      handleSushiDisplay()
+      handleDisplaySetCount()
+    }
+  }, [sushis])
+
+  function handleDisplaySetCount(){
+    setDisplaySetCount(displaySetCount + 1)
+  }
+
+  function handleSushiDisplay(){
+    //calculate the min/max range of IDs to display 4 at a time based on displaySetCount
+    const maxRange = displaySetCount * 4
+    const minRange = maxRange - 3
+
+    const sushiList = sushis.filter((sushi) => {
+        return sushi.id <= maxRange && sushi.id >= minRange
+    })
+    setDisplayedSushis(sushiList)
+    handleDisplaySetCount()
+  }
+
   return (
     <div className="app">
-      <SushiContainer />
+      <SushiContainer sushis={displayedSushis} handleSushiDisplay={handleSushiDisplay}/>
       <Table />
     </div>
   );
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,19 @@
 import React from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi"
 
-function SushiContainer(props) {
+function SushiContainer({ sushis, handleSushiDisplay }) {
+  //const [displayedSushis, setDisplayedSushis] = useState([])
+
+  function handleMoreClick(){
+    //console.log("clicked")
+    handleSushiDisplay()
+  }
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushis.map((sushi) => <Sushi key={sushi.id} name={sushi.name} image={sushi["img_url"]} price={sushi.price} />)}
+      <MoreButton handleMoreClick={handleMoreClick}/>
     </div>
   );
 }
